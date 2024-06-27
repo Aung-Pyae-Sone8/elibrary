2c4aa7a2687346b7c965a179cf490731689fb5b1
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-06-28 02:09:14 +0630

    authentication middleware

--- elibrary/routes/web.php
+++ elibrary/routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminAuth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\user\UserController;
+use App\Http\Controllers\admin\AdminController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware([AdminAuth::class])->group(function() {
+    Route::get('/', function () { return view('welcome'); });
+    Route::get('/login', [AuthController::class, 'loginPage'])->name('auth#loginPage');
+    Route::get('/register', [AuthController::class, 'registerPage'])->name('auth#registerPage');
 });
 
 Route::get('/dashboard', function () {
@@ -15,6 +21,20 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+Route::get('/userHome', [UserController::class, 'index'])->name('user#home');
+
+
+
+    Route::middleware([AdminAuth::class])->group(function() {
+        Route::get('/adminHome', [AdminController::class, 'index'])->name('admin#home');
+
+    });
+
+
 });
+
+
 
 require __DIR__.'/auth.php';
